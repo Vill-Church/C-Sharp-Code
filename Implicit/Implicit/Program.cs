5a7cc70c503a685fd3319c5627c162e7f784b894
William Spires <will@williamspires.com>
2018-01-08 21:18:13 +0000

    first commit

---
+++ Implicit/Implicit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dim say = Console.ReadLine();
+            Console.WriteLine(say.getWord());
+            Console.Read();
+        }
+    }
+    public class Dim
+    {
+        private string word;
+        public Dim(string _word)
+        {
+            word = _word;
+        }
+        public static implicit operator Dim(string _word)
+        {
+            if (_word == null)
+            {
+                return null;
+            }
+            return new Dim(_word);
+        }
+        public string getWord() {
+            return word;
+        }
+    }
+}
